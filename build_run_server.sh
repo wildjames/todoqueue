2fee4bbff56d230e2e912d0dd8f05c116f9922af
James Wild <wild.james343@gmail.com>
2023-09-25 15:15:13 +0100

    Update build_run_server.sh

--- build_run_server.sh
+++ build_run_server.sh
@@ -6,8 +6,7 @@ export DEFAULT_FROM_EMAIL=
 export EMAIL_HOST_PASSWORD=
 
 export DEBUG=0
-export HOSTNAME=localhost
-export PORT=8000
+export DJANGO_HOST_PORT=8000
 
 export DJANGO_DB_NAME=todoqueue
 export DJANGO_DB_USER=root
@@ -19,12 +18,12 @@ export DJANGO_SUPERUSER_EMAIL=
 export DJANGO_SUPERUSER_USERNAME=
 export DJANGO_SUPERUSER_PASSWORD=
 
-# MySQL client. Not necessary in Docker
+# MySQL client. Not necessary in Docker. Only necessary for my specific local dev environment.
 export DYLD_LIBRARY_PATH="/usr/local/mysql-8.0.30-macos12-arm64/lib/:$DYLD_LIBRARY_PATH"
 
 
-cd todoqueue_frontend
-npm run build
+cd todoqueue_frontend && \
+npm run build && \
 
-cd ../todoqueue_backend
+cd ../todoqueue_backend && \
 source ./run_server.sh
