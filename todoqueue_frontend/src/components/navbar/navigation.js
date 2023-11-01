dc5a5e1a172ed2783f0e0bdc458b722a3fcd0303
James Wild <wild.james343@gmail.com>
2023-11-01 09:49:54 +0000

    minor css tweak

--- todoqueue_frontend/src/components/navbar/navigation.js
+++ todoqueue_frontend/src/components/navbar/navigation.js
@@ -77,7 +77,7 @@ export function Navigation({ households, selectedHousehold, setSelectedHousehold
                         )}
                     </Nav>
 
-                    <Nav className="me-right navbar-links-container">
+                    <Nav className="me-right navbar-links-container navbar-links-right">
                         {!isAuth && <Nav.Link href="/signup" className="navbar-link">Sign Up</Nav.Link>}
                         {isAuth ?
                             <Nav.Link href="/logout" className="navbar-link logout-link">Log Out</Nav.Link> :
