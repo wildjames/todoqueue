2a5abc9466976bab16142dd11a6bee98d94f4bbc
James Wild <wild.james343@gmail.com>
2024-11-24 00:21:26 +0000

    Fetch backend url from environment variable

--- todoqueue_frontend/src/api/backend_url.js
+++ todoqueue_frontend/src/api/backend_url.js
@@ -1,2 +1,5 @@
 // export const backend_url = "http://localhost:8000";
-export const backend_url = ""; // For production
+// export const backend_url = ""; // For production
+
+// Get from environment variable
+export const backend_url = process.env.REACT_APP_BACKEND_URL;
