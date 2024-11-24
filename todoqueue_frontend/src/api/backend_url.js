009659b4c608f3e66ea2df492a46e50fdafc6dcd
Jim Wild <wild.james343@gmail.com>
2024-11-24 00:12:21 +0000

    Update backend_url.js

--- todoqueue_frontend/src/api/backend_url.js
+++ todoqueue_frontend/src/api/backend_url.js
@@ -1,2 +1,2 @@
-export const backend_url = "http://localhost:8000";
-// export const backend_url = ""; // For production
+// export const backend_url = "http://localhost:8000";
+export const backend_url = ""; // For production
